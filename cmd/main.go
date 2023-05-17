9453e09ee50653b89de2d34a07247d0090bdba94
aymene01 <aymene01@hotmail.fr>
2023-05-17 17:23:46 +0200

    refacto using switch case

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	bc "github.com/aymene01/go-blockchain/pkg/blockchain"
@@ -24,17 +26,23 @@ func main() {
 
 	memoryBc.AddBlock(genesisBlock)
 
-	for {
-		fmt.Println("Enter sender (or 'q' to quit and 'blocks' to list all the blocks):")
-		var sender string
-		fmt.Scanln(&sender)
+	validCommands := []string{"blocks", "transaction", "q"}
 
-		if sender == "q" {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Println("Enter a command:")
+		if !scanner.Scan() {
+			break
+		}
+		userInput := strings.TrimSpace(scanner.Text())
+
+		switch userInput {
+		case "q":
 			fmt.Println("Exiting...")
 			os.Exit(0)
-		}
 
-		if sender == "blocks" {
+		case "blocks":
 			bc := memoryBc.GetBlockchain()
 
 			for _, block := range bc {
@@ -56,38 +64,54 @@ func main() {
 				}
 			}
 			fmt.Println("-------------------")
-			continue
+
+		case "transaction":
+			fmt.Println("Enter sender:")
+			if !scanner.Scan() {
+				break
+			}
+			sender := strings.TrimSpace(scanner.Text())
+
+			fmt.Println("Enter receiver:")
+			if !scanner.Scan() {
+				break
+			}
+			receiver := strings.TrimSpace(scanner.Text())
+
+			fmt.Println("Enter amount:")
+			if !scanner.Scan() {
+				break
+			}
+			amountStr := strings.TrimSpace(scanner.Text())
+			amount, err := strconv.Atoi(amountStr)
+			if err != nil {
+				fmt.Println("Invalid amount. Please try again.")
+				continue
+			}
+
+			transaction := bc.Transaction{Sender: sender, Receiver: receiver, Amount: amount}
+
+			newBlock := bc.GenerateBlock(memoryBc.Chain[len(memoryBc.Chain)-1], []bc.Transaction{transaction})
+			memoryBc.AddBlock(newBlock)
+
+			fmt.Println("Block added!")
+			fmt.Println("Index:", newBlock.Index)
+			fmt.Println("Timestamp:", newBlock.Timestamp)
+			fmt.Println("Transactions:")
+
+			for _, tx := range newBlock.Transactions {
+				fmt.Println("- Sender:", tx.Sender)
+				fmt.Println("  Receiver:", tx.Receiver)
+				fmt.Println("  Amount:", tx.Amount)
+			}
+			fmt.Println("PrevHash:", newBlock.PrevHash)
+			fmt.Println("Hash:", newBlock.Hash)
+
+		default:
+			fmt.Println("Invalid command. Valid commands are:")
+			for _, command := range validCommands {
+				fmt.Println("-", command)
+			}
 		}
-
-		fmt.Println("Enter receiver:")
-		var receiver string
-		fmt.Scanln(&receiver)
-
-		fmt.Println("Enter amount:")
-		var amountStr string
-		fmt.Scanln(&amountStr)
-		amount, err := strconv.Atoi(amountStr)
-		if err != nil {
-			fmt.Println("Invalid amount. Please try again.")
-			continue
-		}
-
-		transaction := bc.Transaction{Sender: sender, Receiver: receiver, Amount: amount}
-
-		newBlock := bc.GenerateBlock(memoryBc.Chain[len(memoryBc.Chain)-1], []bc.Transaction{transaction})
-		memoryBc.AddBlock(newBlock)
-
-		fmt.Println("Block added!")
-		fmt.Println("Index:", newBlock.Index)
-		fmt.Println("Timestamp:", newBlock.Timestamp)
-		fmt.Println("Transactions:")
-
-		for _, tx := range newBlock.Transactions {
-			fmt.Println("- Sender:", tx.Sender)
-			fmt.Println("  Receiver:", tx.Receiver)
-			fmt.Println("  Amount:", tx.Amount)
-		}
-		fmt.Println("PrevHash:", newBlock.PrevHash)
-		fmt.Println("Hash:", newBlock.Hash)
 	}
 }
